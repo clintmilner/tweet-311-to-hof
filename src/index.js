6d715dbe71a133eebaa2a3bb3ec7afad34fe06c9
Clint Milner <clinton.milner@rebasoft.net>
2019-03-05 09:49:13 +0000

    clean-ups after removing the tag property

--- src/index.js
+++ src/index.js
@@ -10,16 +10,15 @@ log4js.configure({
     categories: { default: { appenders: ['tweet'], level: 'trace' } }
 });
 
-const log = log4js.getLogger('tweet');
-
-const client = new Twitter({
+const log = log4js.getLogger('tweet'),
+    tweets = require('../tweets.json'),
+    canTweet = (process.env.CAN_TWEET && process.env.CAN_TWEET === 'true'),
+    client = new Twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_KEY_SECRET,
     access_token_key: process.env.ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_KEY_SECRET,
-}),
-    tweets = require('../tweets.json'),
-    canTweet = (process.env.CAN_TWEET && process.env.CAN_TWEET === 'true');
+});
 
 let count = process.env.TWEET_START || 0;
 
@@ -54,7 +53,6 @@ const scheduler = NodeSchedule.scheduleJob(process.env.TWEET_TIMER, () => {
 
     } else {
         scheduler.cancel();
-        // count = 0;
         log.warn('All tweets have been published, stopping scheduler');
     }
 });
